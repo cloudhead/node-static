4f1c974b4c1272ea65315e6bceff4d08725b4e85
cloudhead <self@cloudhead.net>
2010-07-27 14:58:15 -0400

    (doc) update example

--- examples/file-server.js
+++ examples/file-server.js
@@ -2,19 +2,23 @@ var sys = require('sys');
 var static = require('../lib/node-static');
 
 //
-// Create a node-static server instance to serve the './public' folder
+// Create a node-static server to serve the current directory
 //
-var file = new(static.Server)();
+var file = new(static.Server)('.', { cache: 7200, headers: {'X-Hello':'World!'} });
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         //
         // Serve files!
         //
-        file.serve(request, response, function (status, headers) {
-            sys.error("> Error serving " + request.url + " - " + status);
-            response.writeHead(status, headers);
-            response.end();
+        file.serve(request, response, function (err, res) {
+            if (err) { // An error as occured
+                sys.error("> Error serving " + request.url + " - " + err.message);
+                response.writeHead(err.status, err.headers);
+                response.end();
+            } else { // The file was served successfully
+                sys.puts("> " + request.url + " - " + res.message);
+            }
         });
     });
 }).listen(8080);
