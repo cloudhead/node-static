11e9782cadff37bd7ae0a510bde81de883193bb6
guybedford <guybedford@gmail.com>
2013-02-04 16:24:17 +0200

    switch to 400 response for malformed uri

--- test/integration/node-static-test.js
+++ test/integration/node-static-test.js
@@ -83,8 +83,8 @@ suite.addBatch({
       topic: function(){
         request.get(TEST_SERVER + '/a%AFc', this.callback);
       },
-      'should respond with 404': function(error, response, body){
-        assert.equal(response.statusCode, 404);
+      'should respond with 400': function(error, response, body){
+        assert.equal(response.statusCode, 400);
       }
     }
 }).addBatch({
