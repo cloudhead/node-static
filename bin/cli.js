3f6cf787f5d6bae711d397918f6ae077134f6648
Gabriel JUCHAULT <gjuchault@users.noreply.github.com>
2018-04-21 09:59:41 +0200

    fix(spa): use strict-equality comparator

--- bin/cli.js
+++ bin/cli.js
@@ -118,7 +118,7 @@ require('http').createServer(function (request, response) {
           }
         };
 
-        if (argv['spa'] && url.parse(request.url).pathname.indexOf(".") == -1) {
+        if (argv['spa'] && url.parse(request.url).pathname.indexOf(".") === -1) {
             file.serveFile(argv['indexFile'], 200, {}, request, response);
         } else {
             file.serve(request, response, callback);
