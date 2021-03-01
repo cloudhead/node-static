9c9389b30caa43c3e2c6f64d5adcad47780b2cde
Bernardo Vieira <bvieira.lists@gmail.com>
2021-03-01 09:47:48 -0300

    Protect fs.stats calls from bad path arguments
    Prevents uncaught exception:
    TypeError [ERR_INVALID_ARG_VALUE]: The argument 'path' must be a string or Uint8Array without null bytes.

--- test/integration/node-static-test.js
+++ test/integration/node-static-test.js
@@ -386,5 +386,14 @@ suite.addBatch({
       assert.equal(body, 'hello world');
     }
   }
+}).addBatch({
+  'handling malicious urls': {
+    topic : function(){
+      request.get(TEST_SERVER + '/%00', this.callback);
+    },
+    'should respond with 404' : function(error, response, body){
+      assert.equal(response.statusCode, 404);
+    }
+  }
 }).export(module);
 
