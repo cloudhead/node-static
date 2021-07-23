2a36af02f0dd831b8562822c3ec750793a562817
Brett Zamir <brettz9@yahoo.com>
2021-07-23 10:44:11 +0800

    feat: switch from neodoc to lighter command-line-basics
    Also updates eslint

--- bin/cli.js
+++ bin/cli.js
@@ -3,11 +3,13 @@
 import http from 'http';
 import fs from 'fs';
 import tty from 'tty';
+import {join, dirname} from 'path';
+import {fileURLToPath} from 'url';
 
-import * as statik from './../lib/node-static.js';
-import neodoc from 'neodoc';
+import {cliBasics} from 'command-line-basics';
 import colors from 'colors/safe.js';
 
+<<<<<<< HEAD
 function help () {
     return `Node-Static CLI - simple, RFC 2616 compliant file streaming module for Node.
 
@@ -100,29 +102,97 @@ const server = http.createServer(function (request, response) {
                 response.writeHead(e.status, e.headers);
                 response.end("Not Found");
                 log(request, response);
+=======
+import * as statik from './../lib/node-static.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+(async () => {
+    const args = await cliBasics(
+        join(__dirname, 'optionDefinitions.js')
+    );
+    if (!args) { // cliBasics handled
+        process.exit(0);
+    }
+
+    const dir = args.directory || '.';
+
+    const log = function(request, response, statusCode) {
+        const d = new Date();
+        const seconds = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds(),
+            minutes = d.getMinutes() < 10 ? '0' + d .getMinutes() : d.getMinutes(),
+            hours   = d.getHours() < 10 ? '0' + d .getHours() : d.getHours(),
+            datestr = hours + ':' + minutes + ':' + seconds,
+
+            line = datestr + ' [' + response.statusCode + ']: ' + request.url;
+        let colorized = line;
+        if (tty.isatty(process.stdout.fd))
+            colorized = (response.statusCode >= 500) ? colors.red.bold(line) :
+                (response.statusCode >= 400) ? colors.red(line) :
+                    line;
+        console.log(colorized);
+    };
+
+    const options = {};
+
+    if ('cache' in args) {
+        options.cache = args['cache']
+    }
+
+    if (args['headers']) {
+        options.headers = JSON.parse(args['headers']);
+    }
+
+    if (args['header-file']) {
+        options.headers = JSON.parse(fs.readFileSync(args['header-file']));
+    }
+
+    if (args['gzip']) {
+        options.gzip = true;
+    }
+
+    if (args['index-file']) {
+        options.indexFile = args['index-file'];
+    }
+
+    const file = new(statik.Server)(dir, options);
+
+    const server = http.createServer(function (request, response) {
+        request.addListener('end', function () {
+            const callback = function(e, rsp) {
+                if (e && e.status === 404) {
+                    response.writeHead(e.status, e.headers);
+                    response.end("Not Found");
+                    log(request, response);
+                } else {
+                    log(request, response);
+                }
+            };
+
+            // Parsing catches:
+            //   npm start -- --spa --index-file test/fixtures/there/index.html
+            //   with http://127.0.0.1:8080/test/fixtures/there?email=john.cena
+            if (args['spa'] && !new URL(request.url, 'http://localhost').pathname.includes(".")) {
+                file.serveFile(args['index-file'], 200, {}, request, response);
+>>>>>>> ea96f50 (feat: switch from neodoc to lighter command-line-basics)
             } else {
-                log(request, response);
+                file.serve(request, response, callback);
             }
-        };
+        }).resume();
+    });
 
-        // Parsing catches:
-        //   npm start -- --spa --indexFile test/fixtures/there/index.html
-        //   with http://127.0.0.1:8080/test/fixtures/there?email=john.cena
-        if (args['spa'] && !new URL(request.url, 'http://localhost').pathname.includes(".")) {
-            file.serveFile(args['--index-file'], 200, {}, request, response);
-        } else {
-            file.serve(request, response, callback);
-        }
-    }).resume();
-});
+    const port = args['port'] || 8080;
+    const hostAddress = args['host-address'] || '127.0.0.1';
 
-if (args['host-address'] === '127.0.0.1') {
-    server.listen(+args['--port']);
-} else {
-    server.listen(+args['--port'], args['--host-address']);
-}
+    if (hostAddress === '127.0.0.1') {
+        server.listen(port);
+    } else {
+        server.listen(port, hostAddress);
+    }
 
-console.log('serving "' + dir + '" at http://' + args['--host-address'] + ':' + args['--port']);
-if (args['--spa']) {
-    console.log('serving as a single page app (all non-file requests redirect to ' + args['--index-file'] +')');
-}
+    console.log('serving "' + dir + '" at http://' + hostAddress + ':' + port);
+    if (args['spa']) {
+        const indexFile = args['index-file'] || 'index.html';
+        console.log('serving as a single page app (all non-file requests redirect to ' + indexFile +')');
+    }
+})();
