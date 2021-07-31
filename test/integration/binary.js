643a528ec7bbd05a59c4030655d94810570afb3f
Brett Zamir <brettz9@yahoo.com>
2021-07-31 11:27:43 +0800

    test: for binary, check file without directory and with custom cache

--- test/integration/binary.js
+++ test/integration/binary.js
@@ -29,7 +29,7 @@ describe('node-static (CLI)', function () {
         beforeEach(async function () {
             await updatePort(this);
         });
-        it('serving hello.txt', async function () {
+        it('serving file within directory', async function () {
             const {response /* , stdout */} = await spawnConditional(binFile, [
                 '-p', this.port, fixturePath
             ], timeout - 9000, {
@@ -48,6 +48,68 @@ describe('node-static (CLI)', function () {
             assert.equal(status, 200, 'should respond with 200');
             assert.equal(contentType, 'text/plain', 'should respond with text/plain');
             assert.equal(text, 'hello world', 'should respond with hello world');
+        });
+
+        it('serving file without directory', async function () {
+            const {response /* , stdout */} = await spawnConditional(binFile, [
+                '-p', this.port
+            ], timeout - 9000, {
+                condition: /serving "."/,
+                action: (/* err, stdout */) => {
+                    return fetch(
+                        `http://localhost:${this.port}/test/fixtures/hello.txt`
+                    );
+                }
+            });
+
+            const {status} = response;
+            const contentType = response.headers.get('content-type');
+            const cacheControl = response.headers.get('cache-control');
+            const text = await response.text();
+
+            assert.equal(status, 200, 'should respond with 200');
+            assert.equal(contentType, 'text/plain', 'should respond with text/plain');
+            assert.equal(text, 'hello world', 'should respond with hello world');
+            assert.equal(cacheControl, 'max-age=3600', 'should respond with cache-control');
+        });
+
+        it('serves custom cache', async function () {
+            const {response: responses /* , stdout */} = await spawnConditional(binFile, [
+                '-p', this.port, '--cache', JSON.stringify({
+                    '**/*.txt': 100,
+                    '**/': 300
+                }), fixturePath
+            ], timeout - 9000, {
+                condition: /serving ".*?"/,
+                action: (/* err, stdout */) => {
+                    return Promise.all([
+                        fetch(
+                            `http://localhost:${this.port}/`
+                        ),
+                        fetch(
+                            `http://localhost:${this.port}/hello.txt`
+                        ),
+                        fetch(
+                            `http://localhost:${this.port}/empty.css`
+                        )
+                    ]);
+                }
+            });
+
+            [
+                ['max-age=300'],
+                ['max-age=100'],
+                [undefined]
+            ].forEach(([expectedCacheControl], i) => {
+                const response = responses[i];
+                const {status} = response;
+                const cacheControl = response.headers.get('cache-control');
+                // const contentType = response.headers.get('content-type');
+                // const text = await response.text();
+
+                assert.equal(status, 200, 'should respond with 200');
+                assert.equal(cacheControl, expectedCacheControl, 'should respond with cache-control or lack');
+            });
         });
     });
 });
