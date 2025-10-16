95ddb72c87961b41f2a512c034e2f2be81833870
Brett Zamir <brettz9@yahoo.com>
2025-10-15 21:06:44 -0700

    chore: use Zlib for gzip demo

--- test/gzip.js
+++ test/gzip.js
@@ -1,7 +1,22 @@
-import {writeFile} from 'fs/promises';
-import {gzip} from 'node-gzip';
+import {
+    createReadStream,
+    createWriteStream,
+} from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
 
-await writeFile(
-    import.meta.dirname + '/fixtures/hello.txt.gz',
-    await gzip('hello world')
+/**
+ * @param {string} input
+ * @param {string} output
+ */
+async function gzip (input, output) {
+    const gzip = createGzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    await pipeline(source, gzip, destination);
+}
+
+await gzip(
+    import.meta.dirname + '/fixtures/hello.txt',
+    import.meta.dirname + '/fixtures/hello.txt.gz'
 );
